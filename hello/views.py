62bc8feded09bc59c7440b271113c82567ec68c4
Tatsuya-kanaria <kanaria.git@gmail.com>
2021-06-11 10:35:15 +0900

    urls.py の修正

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,7 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello Django!!")
+    return HttpResponse("Hello Django!!!")
