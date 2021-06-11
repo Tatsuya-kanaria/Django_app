d4d9ad8e73b258ac776ae03a825e2b54683a3a3b
Tatsuya-kanaria <kanaria.git@gmail.com>
2021-06-11 22:42:52 +0900

    クエリパラメータを利用する

--- hello/views.py
+++ hello/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello Django!!!")
+def index(request, id, nickname):
+    result = 'your id: ' + str(id) + ', name: "' \
+        + nickname + '".'
+    return HttpResponse(result)
