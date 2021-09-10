08dae378183bb6a1c7ebc7dfac88b59baae773f3
Tatsuya-kanaria <kanaria.git@gmail.com>
2021-09-10 19:21:10 +0900

    finish 2-4

--- hello/forms.py
+++ hello/forms.py
@@ -8,5 +8,26 @@ class HelloForm(forms.Form):
     #                        widget=forms.TextInput(attrs={'class': 'form-control'}))
     # age = forms.IntegerField(label='age',
     #                          widget=forms.NumberInput(attrs={'class': 'form-control'}))
+
     # required True:チェックしないと送信できない
-    check = forms.BooleanField(label='check', required=False)
+    # check = forms.BooleanField(label='check', required=False)
+
+    # check = forms.NullBooleanField(label='Check')
+
+    data = [
+        ('one', 'item 1'),
+        ('two', 'item 2'),
+        ('three', 'item 3'),
+        ('four', 'item 4'),
+        ('five', 'item 5')
+    ]
+    # choice = forms.ChoiceField(label='Choice', choices=data)
+
+    # choice = forms.ChoiceField(label='radio', \
+    #     choices=data, widget=forms.RadioSelect())
+
+    # choice = forms.ChoiceField(label='radio', \
+    #     choices=data, widget=forms.Select(attrs={'size': 5}))
+
+    choice = forms.MultipleChoiceField(
+        label='radio', choices=data, widget=forms.SelectMultiple(attrs={'size': 6}))
--- hello/views.py
+++ hello/views.py
@@ -10,22 +10,43 @@ class HelloView(TemplateView):
     def __init__(self):
         self.params = {
             'title': 'Hello',
-            'message': 'your data:',
-            'form': HelloForm()
+            # 'message': 'your data:',
+            'form': HelloForm(),
+            'result': None
         }
 
     def get(self, request):
         return render(request, 'hello/index.html', self.params)
 
     def post(self, request):
-        if ('check' in request.POST):
-            self.params['result'] = 'Checked!!'
-        else:
-            self.params['result'] = 'Not checked...'
+        # form
         # msg = 'あなたは、<b>' + request.POST['name'] + \
         #     '(' + request.POST['age'] + \
         #     ') </b>さんです。<br>メールアドレスは <b>' + request.POST['mail'] + \
         #     '</b> ですね。'
         # self.params['message'] = msg
+
+        # BooleanField checkbox
+        # if ('check' in request.POST):
+        #     self.params['result'] = 'Checked!!'
+        # else:
+        #     self.params['result'] = 'Not checked...'
+
+        # NullBooleanField
+        # chk = request.POST['check']
+        # self.params['result'] = 'you selected: "' + chk + '".'
+
+        # ChoiceField pulldownmenu, radiobuttons
+        # ch = request.POST['choice']
+        # self.params['result'] = 'selected: "' + ch + '".'
+
+        # MultipleChoiceField getlist
+        ch = request.POST.getlist('choice')
+        result = '<ol class="list-group"><b>selected:</b>'
+        for item in ch:
+            result += '<li class="list-group-item">' + item + '</li>'
+        result += '</ol>'
+        self.params['result'] = result
+
         self.params['form'] = HelloForm(request.POST)
         return render(request, 'hello/index.html', self.params)
